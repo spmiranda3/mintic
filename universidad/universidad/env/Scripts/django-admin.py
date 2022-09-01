962a448d22795de62d656f0ed76f77e6ac4ea001
spmiranda3 <spmiranda3@misena.edu.co>
2022-09-01 06:55:52 -0500

    Proyecto

---
+++ universidad/universidad/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\sandra\sandra\UNIVERSIDAD\universidad\universidad\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
